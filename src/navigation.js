e27502b53f3a40d187a42793b2121c21aa7a98e9
Elena <dergaeva.elena@mail.ru>
2017-11-15 21:28:52 +0200

    1-12

---
+++ src/navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export const Navigation = () => (
+	<nav>
+		<ul>
+			<li>Home</li>
+			<li>Gallery</li>
+		</ul>
+	</nav>
+);
+
+// React.createElement
