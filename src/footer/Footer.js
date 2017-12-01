71c1911df156fff8418c73a3e18718cd17dac7f0
Olena Dergaieva <olena.dergaieva@synapse.com>
2017-12-01 18:56:38 +0200

    01-12-2017

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './footer.scss';
 
 import { Username } from './userList';
+import { users } from './userList';
+import { ListComponentName } from './userList';
 
 export const Footer = () => (
-    <footer className="footer">
-        <Username fName='Dergaieva'  lName='Elena' age='30' />
-    </footer>
+  <footer className="footer">
+    <Username fName='Dergaieva'  lName='Elena' age='30' />
+    <ListComponentName usersList={users} />
+  </footer>
 );
--- src/footer/userList/userList.js
+++ src/footer/userList/userList.js
@@ -2,12 +2,41 @@ import React from 'react';
 
 import './userList.scss';
 
+export const users =  [
+  {
+    fName: "Gortovskiy",
+    lName: "Oleg",
+    age: "50",
+    id: 1
+  },
+  {
+    fName: "Ivanova",
+    lName: "Olga",
+    age: "40",
+    id: 2
+  },
+  {
+    fName: "Trydgg",
+    lName: "Olena",
+    age: "30",
+    id: 1
+  },
+];
+
 export const Username = (props) => {
-    return (
-        <div>
-            <span>{props.fName} </span>
-            <span>{props.lName} </span>
-            <span>{props.age} </span>
-        </div>
-    );
+  return (
+    <div class="name">
+      <span>{props.fName} </span>
+      <span>{props.lName} </span>
+      <span>{props.age} </span>
+    </div>
+  );
 };
+
+export const ListComponentName = (props) => {
+  return (
+    <div class="name">
+    <ul>{
+      props.usersList.map(user => <li key={user.id}>{user.fName} {user.lName} {user.age}</li>)
+    }</ul></div>
+  )};
