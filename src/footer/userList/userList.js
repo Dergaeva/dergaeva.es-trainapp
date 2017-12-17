e02d9efcbe17e96eba12de45ff19abdd088c0089
Dergaeva <dergaeva.elena@mail.ru>
2017-12-17 22:41:02 +0200

    eslint changes-2

--- src/footer/userList/userList.js
+++ src/footer/userList/userList.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import './userList.scss';
 
-export const Username = (props) => {
-  return (
-    <div className="name">
-      <span>{props.fName} </span>
-      <span>{props.lName} </span>
-      <span>{props.age} </span>
-    </div>
-  )
-};
+export const Username = props => (
+  <div className="name">
+    <span>{props.fName} </span>
+    <span>{props.lName} </span>
+    <span>{props.age} </span>
+  </div>
+);
 
-export const ListComponentName = (props) => {
-  return (
-    <div className="name">
-      <ul>{
-        props.usersList.map(user =>
-          <li key={user.id}>
-            <Username
-              fName={user.fName}
-              lName={user.lName}
-              age={user.age}
-            />
-          </li>)
-      }
-      </ul>
-    </div>
-  );
-};
+export const ListComponentName = props => (
+  <div className="name">
+    <ul>{
+      props.usersList.map(user => (
+        <li key={user.id}>
+          <Username
+            fName={user.fName}
+            lName={user.lName}
+            age={user.age}
+          />
+        </li>
+      )
+      )
+    }
+    </ul>
+  </div>
+);
