7d0e009992839e2ccfcf4abf5b0172660d839b8c
Elena <dergaeva.elena@mail.ru>
2018-01-08 16:44:14 +0200

    'vers-11'

--- src/pages/Task/Task.js
+++ src/pages/Task/Task.js
@@ -1,4 +1,43 @@
-import { updateTask, getTask, createTask } from 'services/tasksService';
+// import { updateTask, getTask, createTask } from 'services/tasksService';
+//
+// export class Task extends Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       title: 'New',
+//       description: '',
+//       day: props.location.search.replace(/\D+/, '')
+//     };
+//   }
+//
+//   componentDidMount() {
+//     const { task } = this.props.match.params;
+//
+//     if (task !== 'new') {
+//       getTask(task)
+//         .then(task => this.setState(Object.assign({}, task)));
+//     }
+//   }
+//
+//   updateTask = () => {
+//     const { task } = this.props.match.params;
+//     const taskData = this.state.task;
+//
+//     if (task !== 'new') {
+//       updateTask(task, taskData);
+//     } else {
+//       createTask(taskData);
+//     }
+//   }
+//
+//   render() {
+//     const { title } = this.state;
+//     return <strong>Task "{title}"</strong>;
+//   }
+// }
+
+
 
 export class Task extends Component {
   constructor(props) {
@@ -20,19 +59,52 @@ export class Task extends Component {
     }
   }
 
-  updateTask = () => {
+  updateTasks = (e) => {
     const { task } = this.props.match.params;
-    const taskData = this.state.task;
+    let promise;
 
     if (task !== 'new') {
-      updateTask(task, taskData);
+      promise = updateTask(task, this.state);
     } else {
-      createTask(taskData);
+      promise = createTask(this.state);
     }
-  }
+
+    promise.then(() => this.props.history.push('/tasks'));
+    e.preventDefault();
+  };
+
+  onChange = (e) => {
+    const { target } = e;
+
+    this.setState({ [target.name]: target.value });
+  };
 
   render() {
-    const { title } = this.state;
-    return <strong>Task "{title}"</strong>;
+    const { title, description } = this.state;
+
+    return (
+      <form onSubmit={this.updateTasks}>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={this.onChange}
+          placeholder="Enter a title"
+        />
+        <br />
+        <textarea
+          name="description"
+          cols="30"
+          rows="10"
+          value={description}
+          onChange={this.onChange}
+        />
+        <br />
+        <input
+          type="submit"
+          value="Save"
+        />
+      </form>
+    );
   }
 }
